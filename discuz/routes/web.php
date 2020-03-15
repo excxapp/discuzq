81ba77b51cb429d905882a3755f52e2788196091
chen <zhjay999@163.com>
2020-03-15 21:22:28 +0800

    Revert "init"
    This reverts commit 324c65d2547bc01f638bff34f41746610f796987.

--- discuz/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Install\Controller as InstallController;
-
-$route->get('/install', 'install.index', InstallController\IndexController::class);
-$route->post('/install', 'install', InstallController\InstallController::class);
-
